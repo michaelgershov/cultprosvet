8be1ce25f8fd69b4d1944fdfd00cee008ec915ba
Gershov Michael <michaelgershov@yandex.by>
2025-06-17 16:48:39 +0300

    Cosmetic edits

--- about.py
+++ about.py
@@ -7,15 +7,15 @@ st.write("""
     Приложение "Культпросвет" на данный момент является минимально жизнеспособной версией более масштабного проекта,
     направленного на культурное просвещение населения.
     
-    Основной источник данных для приложения – интернет-платформа afisha.relax.by:
-""")
+    Основной источник данных для приложения – интернет-платформа <a href="https://afisha.relax.by" target="_blank">afisha.relax.by</a>:
+""", unsafe_allow_html=True)
 
-st.image("images/relax.png", caption="afisha.relax.by", use_column_width=True)
+st.image("images/relax.png", use_container_width=True)
 
 
 st.write("""
-    Дополнительный источник данных для приложения – база данных themoviedb.org (TMDb):
-""")
+    Дополнительный источник данных для приложения – база данных <a href="https://www.themoviedb.org" target="_blank">themoviedb.org</a> (TMDb):
+""", unsafe_allow_html=True)
 
-st.image("images/tmdb.svg", caption="themoviedb.org", use_column_width=True)
+st.image("images/tmdb.svg", use_container_width=True)
 
